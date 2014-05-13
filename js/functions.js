6a51644c498f260ecbd2fb2ef08bd6ed66c5a6a8
hvaras <king.cold@gmail.com>
2014-05-13 17:09:08 -0400

    pruebas

--- js/functions.js
+++ js/functions.js
@@ -21,12 +21,12 @@ $(function() {
         img.src = "http://images.forwallpaper.com/files/thumbs/preview/8/84534__snow-frost-window_p.jpg";
 
         function drawPoint(pointX,pointY){
-            var grd = ctx.createRadialGradient(pointX, pointY, 0, pointX, pointY, 10);
+            var grd = ctx.createRadialGradient(pointX, pointY, 0, pointX, pointY, 50);
             grd.addColorStop(0, "rgba(255,255,255,.6)");
             grd.addColorStop(1, "transparent");
             ctx.fillStyle = grd;
             ctx.beginPath();
-            ctx.arc(pointX,pointY,10,0,Math.PI*1,true);
+            ctx.arc(pointX,pointY,50,0,Math.PI*2,true);
             ctx.fill();
             ctx.closePath();
         }
